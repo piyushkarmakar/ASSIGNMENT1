91b435cdfd01b856a0be990ff0438bb5de5325d1
piyush karmakar <79916156+piyushkarmakar@users.noreply.github.com>
2023-07-13 17:39:47 +0530

    Create terraform_bucket.tf

---
+++ terraform_bucket.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+
+access_key = "AKIAZ3ISJZDWPGLZLCER"
+secret_key = "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"
+region = "eu-north-1"
+
+}
+
+resource "aws_s3_bucket" "second" {
+
+bucket = "ldtech.s3.bucket.pkr49003"
+
+
+}
